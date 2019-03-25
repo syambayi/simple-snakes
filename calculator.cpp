6123f546c95772a43a68841d0727d29be1f2877d
syambayi <48857065+syambayi@users.noreply.github.com>
2019-03-25 17:57:05 +0000

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#Using namspace std;
+ int main ()  
+{
+int a,b
+cout<<"please enter the first number"<<endl;
+cin>>A
+cout<<" please ent the second number"<<endl;
+cin>>B
+ int sum=A+B;
+ int product=B*A;
+ int substration=B-A;
+ int devision=B/A;
+ /*% of B/A */
+ cout<<"the sum is"<<sum<<endl;
+ cout<<"the product is"<<product<<endl;
+ cout<<"the substration is"<<substraction<<endl;
+ cout<<"the devision is"<<devision<<endl;
+ cout<<"the modulos is"<<modulos<<endl;
+ 
+ return 0;
+ 
+	
+}
